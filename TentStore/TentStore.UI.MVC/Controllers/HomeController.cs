c7e0234973bf9aa7481ce88c06dcb2d491d3f263
BryanClingan <BryanClingan@outlook.com>
2021-02-09 13:24:23 -0600

    some template conversion completed added contact form added login functionality

--- TentStore/TentStore.UI.MVC/Controllers/HomeController.cs
+++ TentStore/TentStore.UI.MVC/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TentStore.UI.MVC.Models;
+using System.Net;
+using System.Net.Mail;
 
 
 
@@ -15,18 +18,70 @@ namespace TentStore.UI.MVC.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Shop()
         {
-            ViewBag.Message = "Your application description page.";
-
+            return View();
+        }
+        public ActionResult Detail()
+        {
+            return View();
+        }
+        public ActionResult Cart()
+        {
+            return View();
+        }
+        public ActionResult Checkout()
+        {
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            string message = $"You have received an email from {cvm.Name} with a subject of {cvm.Subject}. Please respond to {cvm.Email} with your response to the following message: <br/>{cvm.Message}";
+
+            MailMessage mm = new MailMessage("webadmin@bryandoescode.com", "bryanclingan@outlook.com", cvm.Subject, message);
+
+            mm.IsBodyHtml = true;
+
+            mm.Priority = MailPriority.High;
+
+            mm.ReplyToList.Add(cvm.Email);
+
+            SmtpClient client = new SmtpClient("mail.bryandoescode.com");
+
+            client.Credentials = new NetworkCredential("webadmin@bryandoescode.com", "Password!!!111");
+
+            client.Port = 587;
+            //client.UseDefaultCredentials = false;
+            //client.EnableSsl = false;
+
+            try
+            {
+                client.Send(mm);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.CustomerMessage = $"We're sorry your request cannot be sent at this time. Please try again later. <br/>ErrorMessage:<br/> {ex.StackTrace}";
+                return View(cvm);
+            }
+            return View("EmailConfirmation", cvm);
+
+        }
+
+
+
     }
 }
---
+++ TentStore/TentStore.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TentStore.UI.MVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "* Please provide a Name* ")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "* Please provide a Email *")]
+        [DataType(DataType.EmailAddress)]
+        
+        public string Email { get; set; }
+
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "* Please provide a Message *")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
+    }
+}
