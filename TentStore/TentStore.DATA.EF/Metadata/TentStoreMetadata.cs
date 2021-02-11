02f388e9388c6da7ae860dca688425f152954d8d
BryanClingan <BryanClingan@outlook.com>
2021-02-10 22:46:55 -0600

    added metadata folder startedd adding metadata

---
+++ TentStore/TentStore.DATA.EF/Metadata/TentStoreMetadata.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TentStore.DATA.EF
+{
+
+    #region Brand Metadata
+    public class BrandMetadata
+    {
+        [Required(ErrorMessage ="*Required")]
+        [StringLength(25,ErrorMessage ="*25 characters or less")]
+        [Display(Name ="Brand")]
+        public string BrandName { get; set; }
+    }
+
+    [MetadataType(typeof(BrandMetadata))]
+    public partial class Brand
+    {
+
+    }
+
+    #endregion
+
+    #region Capacity Metadata
+    public class CapacityMetadata
+    {
+        [Required(ErrorMessage ="*Required")]
+        [StringLength(10,ErrorMessage ="*10 characters or less")]
+        public string Capacity1 { get; set; }
+    }
+    [MetadataType(typeof(CapacityMetadata))]
+    public partial class Capacity
+    {
+
+    }
+    #endregion
+
+    #region Dimension Metadata
+    public class DimensionMetadata
+    {
+        [Required(ErrorMessage = "*Required")]
+        [StringLength(25, ErrorMessage = "*25 characters or less")]
+        [Display(Name ="Dimensions")]
+        public string DimensionRange { get; set; }
+    }
+    [MetadataType(typeof(DimensionMetadata))]
+    public partial class Dimension
+    {
+
+    }
+    #endregion
+
+    #region Manufacturer Metadata
+    public class ManufacturerMetadata
+    {
+        [Required(ErrorMessage ="*Required")]
+        [Display(Name="Manufacturer")]
+        [StringLength(100,ErrorMessage ="*100 characters or less")]
+        public string ManufacturerName { get; set; }
+        [Required(ErrorMessage = "*Required")]
+        [StringLength(25, ErrorMessage = "*25 characters or less")]
+        public string City { get; set; }
+        [DisplayFormat(NullDisplayText =  "[N/A]")]
+        [StringLength(2,MinimumLength =2, ErrorMessage = "*2 characters")]
+        public string State { get; set; }
+        [Required(ErrorMessage = "*Required")]
+        [StringLength(25, ErrorMessage = "*25 characters or less")]
+        public string Country { get; set; }
+    }
+    [MetadataType(typeof(ManufacturerMetadata))]
+    public partial class Manufacturer
+    {
+
+    }
+    #endregion
+
+    #region PoleMaterial Metadata
+    public class PoleMaterialMetadata
+    {
+        [Required(ErrorMessage ="*Required")]
+        [Display(Name ="Pole Material")]
+        [StringLength(20,ErrorMessage ="*20 characters or less.")]
+        public string PoleMaterial1 { get; set; }
+    }
+    [MetadataType(typeof(PoleMaterialMetadata))]
+    public partial class PoleMaterial
+    {
+
+    }
+    #endregion
+
+    #region Season Metadata
+    public class SeasonMetadata
+    {
+        [Required(ErrorMessage = "*Required")]
+        [Display(Name = "Season")]
+        [StringLength(100, ErrorMessage = "*100 characters or less.")]
+        public string Season1 { get; set; }
+    }
+    [MetadataType(typeof(SeasonMetadata))]
+    public partial class Season
+    {
+
+    }
+    #endregion
+
+    #region StatusID Metadata
+    public class StatusIDMetadata
+    {
+        [Required(ErrorMessage ="*Required")]
+        [StringLength(20,ErrorMessage ="*20 characters or less")]
+        public string Status { get; set; }
+    }
+    [MetadataType(typeof(StatusIDMetadata))]
+    public partial class StatusID
+    {
+
+    }
+    #endregion
+
+    #region Tent Metadata
+    public class TentMetadata
+    {
+        public string Name { get; set; }
+        public int BrandID { get; set; }
+        public int StatusID { get; set; }
+        public int ManufacturerID { get; set; }
+        public int DimensionsID { get; set; }
+        public int CapacityID { get; set; }
+        public int TentMaterialID { get; set; }
+        public int PoleMaterialID { get; set; }
+        public decimal Price { get; set; }
+        public int SeasonID { get; set; }
+        public decimal Weight { get; set; }
+        public bool isWaterProof { get; set; }
+        public bool isDiscontinued { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+    }
+    [MetadataType(typeof(TentMetadata))]
+    public partial class Tent
+    {
+
+    }
+    #endregion 
+    //not done
+    #region TentMaterial Metadata
+    public class TentMaterialMetadata
+    {
+        public string TentMaterial1 { get; set; }
+    }
+    [MetadataType(typeof(TentMaterialMetadata))]
+    public partial class TentMaterial
+    {
+
+    }
+    #endregion
+    //not done
+    #region Department Metadata
+    public class DepartmentMetadata
+    {
+        public string DeptName { get; set; }
+    }
+    [MetadataType(typeof(DepartmentMetadata))]
+    public partial class Department
+    {
+
+    }
+    #endregion
+    //not done
+    #region Employee Metadata
+    public class EmployeeMetadata
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+        public Nullable<int> ReportsTo { get; set; }
+        public int DeptID { get; set; }
+    }
+    [MetadataType(typeof(EmployeeMetadata))]
+    public partial class Employee
+    {
+
+    }
+    #endregion 
+    //not done
+}
